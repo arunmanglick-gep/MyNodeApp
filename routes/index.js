b65fd47f68c3b93537b76609d0bb0a6dd71ecb91
root <root@ip-172-31-23-131.ec2.internal>
2017-04-29 02:57:47 +0000

    Added minor change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express App (Check Node.Js Installation Over AWS Linux AMI EC2)' });
+  res.render('index', { title: 'Express App (Check Node.Js Installation Over AWS Linux AMI EC2-Server 1)' });
 });
 
 module.exports = router;
